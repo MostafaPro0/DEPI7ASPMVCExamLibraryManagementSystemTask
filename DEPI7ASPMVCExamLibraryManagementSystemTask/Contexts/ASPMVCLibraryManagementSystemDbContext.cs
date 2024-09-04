aed007b29c3ea2dc1efc871c38b09b22c3abb383
MostafaPro0 <MostafaPro0@yahoo.com>
2024-09-04 22:28:17 +0300

    Add project files.

---
+++ DEPI7ASPMVCExamLibraryManagementSystemTask/Contexts/ASPMVCLibraryManagementSystemDbContext.cs
@@ -0,0 +1,32 @@
+﻿using DEPI7ASPMVCExamLibraryManagementSystemTask.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DEPI7ASPMVCExamLibraryManagementSystemTask;
+
+namespace DEPI7ASPMVCExamLibraryManagementSystemTask.Contexts
+{
+    public class DEPI7ASPMVCExamLibraryManagementSystemDbContext:DbContext 
+    {
+
+        public DEPI7ASPMVCExamLibraryManagementSystemDbContext()
+        {
+
+        }
+        public DEPI7ASPMVCExamLibraryManagementSystemDbContext(DbContextOptions<DEPI7ASPMVCExamLibraryManagementSystemDbContext>options):base(options)
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(LocalDb)\MSSQLLocalDB; Database=BookCRUDMVCApp;Trusted_Connection=true;");
+            base.OnConfiguring(optionsBuilder);
+        }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ DEPI7ASPMVCExamLibraryManagementSystemTask/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DEPI7ASPMVCExamLibraryManagementSystemTask.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
